cb0f009030fe229bd76e1578850a2376bf853a35
SakethDodda12 <sakethdodda57@gmail.com>
2025-10-03 20:36:13 -0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+def getRandomDate(startDate, endDate):
+    print("Printing random date between", startDate, "and", endDate)
+    randomGenerator = random.random()
+    dateFormat = '%d/%m/%Y'
+    starttime = time.mktime(time.strptime(startDate, dateFormat))
+    endtime = time.mktime(time.strptime(endDate, dateFormat))
+    randomTime = starttime + randomGenerator * (endtime - starttime)
+    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
+    return randomDate
+
+print("Random date =", getRandomDate("1/1/2020", "12/12/2024"))
